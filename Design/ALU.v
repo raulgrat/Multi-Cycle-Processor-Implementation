3d68e2bdb0fb1904c7b5a2fccd2f07c66e491112
raulgrat <156843109+raulgrat@users.noreply.github.com>
2024-11-04 14:56:58 -0500

    Update ALU.v

--- Design/ALU.v
+++ Design/ALU.v
@@ -15,22 +15,12 @@ module ALU(
     output wire [31:0] op_0,
     output wire change_pc
     );
-    
-    // Use assign statement to perform add, sub,
-    // and, or operations on ip_0 and ip_1 based 
-    // on the instruction/opcode
-    
-//    assign op_0 = 
+
     assign op_0 = (opcode == 4) ? (ip_0 + ip_1):(
                   (opcode == 5) ? (ip_0 - ip_1):(
                   (opcode == 6) ? (ip_0 & ip_1):(
                   (opcode == 7) ? (ip_0 | ip_1):0)));
     
-    
-    // Change_pc is required for the branch operations for 
-    // the two control instructions. If ip_0 and ip_1 are equal 
-    //  when the instruction is "beq" or ip_0 < ip_1 when 
-    // instruction is "blt", the change_pc goes high 
     
     assign change_pc = opcode == 3'b010 ? (ip_0 == ip_1):(
                         opcode == 3'b011 ? (ip_0 < ip_1) : 0);
